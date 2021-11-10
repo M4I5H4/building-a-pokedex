14cf16ab923fd353d8d2f10f52cd7cdee41ec6b6
M4I5HA <maisha.adamsam@gmail.com>
2021-11-10 23:19:56 +0000

    added footer with links

---
+++ Footer.js
@@ -0,0 +1,14 @@
+const footer = document.getElementById("footer");
+footer.innerHTML = "<nav><a href='#'>Back to Top</a>  |  <a href='https://github.com/M4I5H4/building-a-pokedex'>GitHub</a>  |  <a href='https://maishaadams.netlify.app'>About Me</a><nav/>"
+console.log(footer)
+// const addFooter = () => {
+//     return (
+//        `<div>
+//         <h1>does this work</h1>
+//        </div>`
+//     )
+       
+    
+// }
+
+
