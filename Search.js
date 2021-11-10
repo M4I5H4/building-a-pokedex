d48fef58661fce50fea7b67fd7c036ab454454dd
M4I5HA <maisha.adamsam@gmail.com>
2021-11-10 21:33:38 +0000

    added more style features

---
+++ Search.js
@@ -0,0 +1,12 @@
+const searchBar = document.getElementById('searchBar');
+let pokemonCharacters = [];
+searchBar.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+
+    
+
+
+
+
+
+});
