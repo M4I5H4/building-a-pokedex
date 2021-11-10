10b8a3c1a8e1eae98f838e7ff118a24a2f896db2
M4I5HA <maisha.adamsam@gmail.com>
2021-11-10 17:06:12 +0000

    added pokemon to page - no styling

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-
+const pokedex = document.getElementById("pokedex");
+console.log(pokedex);
 //this is the fucntion to get the pokemon
 const fetchPokemon = () => {
     console.log('Fetching Pokemon')// checking that the function is working
 
-    const url = `https://pokeapi.co/api/v2/pokemon/1`
-    fetch(url)
-        .then( res => {
-            return res.json();
-        })
-        .then( data => {
-            console.log(data);
-
-            const pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites.front_default,
-                type: data.types
-                .map( (type) => type.type.name) //types is an array so we use the map fuction to loop through
-                .join(', ')
-            };
-                console.log(pokemon)
+    const promises = [];
+    for (let i = 1; i <= 150; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+        promises.push(fetch(url).then( (res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites.front_default,
+            type: data.types
+            .map( (type) => type.type.name) //types is an array so we use the map fuction to loop through
+            .join(', ')
+    }));
+        displayPokemon(pokemon)
         });
 
-
 };
+const displayPokemon =(pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map (pokeman =>
+        `<li>
+            <img src="${pokeman.image}"/>
+            <h2>${pokeman.id}. ${pokeman.name}</h2>
+            <p>Type: ${pokeman.type}</p>
+        </li>`
+        )
+    pokedex.innerHTML = pokemonHTMLString
+}
 
-fetchPokemon(); //calling the fution
+fetchPokemon(); //calling the function
 
